4b385229829375faa21c3bcb40d137893c612784
mujib-programmer <mujib.programmer@gmail.com>
2018-08-28 11:00:07 +0700

    learn concurency

---
+++ go-basic-knowledge/concurency_timeout.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	c := make(chan int)
+	o := make(chan bool)
+	go func() {
+		for {
+			select {
+			case v := <-c:
+				fmt.Println(v)
+			case <-time.After(5 * time.Second):
+				fmt.Println("timeout")
+				o <- true
+				break
+			}
+		}
+	}()
+	<-o
+}
