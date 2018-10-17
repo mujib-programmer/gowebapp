b25c64f9f3dfafff31dd6a7702bced8a03410621
mujib-programmer <mujib.programmer@gmail.com>
2018-10-17 07:51:48 +0700

    working with strings

---
+++ text-files/strings/convert_to_string.go
@@ -0,0 +1,14 @@
+// Format series, convert data types into string
+package main
+
+import "fmt"
+import "strconv"
+
+func main() {
+	a := strconv.FormatBool(false)
+	b := strconv.FormatFloat(123.23, 'g', 12, 64)
+	c := strconv.FormatInt(1234, 10)
+	d := strconv.FormatUint(12345, 10)
+	e := strconv.Itoa(1023)
+	fmt.Println(a, b, c, d, e)
+}
