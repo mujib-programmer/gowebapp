0c03074caac29b5ab442c5d63534dcc249657e67
mujib-programmer <mujib.programmer@gmail.com>
2018-10-22 09:43:29 +0700

    working with http rpc server and client

---
+++ web-services/rpc/http_rpc_client.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+	"os"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "server")
+		os.Exit(1)
+	}
+
+	serverAddress := os.Args[1]
+
+	client, err := rpc.DialHTTP("tcp", serverAddress+":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	// Syncronous call
+	args := Args{17, 8}
+	var reply int
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d8%d=%d\n", args.A, args.B, reply)
+
+	var quot Quotient
+	err = client.Call("Arith.Divide", args, &quot)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d/%d=%d remainder %d\n", args.A, args.B, quot.Quo, quot.Rem)
+}
+
+// We compile the client and the server side code separately then start the server and client. You'll then have something
+// similar as follows after you input some data.
+
+/*
+$ ./http_rpc_client localhost
+Arith: 17*8=136
+Arith: 17/8=2 remainder 1
+*/
