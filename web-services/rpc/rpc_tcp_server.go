c2ca68393f09226fc17d09e03deaf00eb4acaf2a
mujib-programmer <mujib.programmer@gmail.com>
2018-10-24 08:12:04 +0700

    working with REST tcp and json client server

---
+++ web-services/rpc/rpc_tcp_server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("Divide by zero")
+	}
+
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+
+func main() {
+	arith := new(Arith)
+	rpc.Register(arith)
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":1234")
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		rpc.ServeConn(conn)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
